30c76528eb6584aa78844de7e0c601bca899cfaa
RayL1991 <rayliurui@foxmail.com>
2020-08-18 23:00:36 +0930

    update

--- week03/SquareCityWalking.java
+++ week03/SquareCityWalking.java
@@ -36,16 +36,49 @@ Find and return the largest possible similarity of cultures on a valid route fro
 
 public class SquareCityWalking{
 	public static int largestGCD(int N, int[] A){
-		int largestGCD = 0;
-		int i = 0;
-		int j = 0;
 
-
-
+		int[] route = new int[N*N];
+		route[0] = A[0];
+		for (int i = 2; i<N-1; i++) {
+			route[i] = getGCD(route[i - 1], A[i]);
+		}
+		for (int i = 2; i< N-1; i++){
+			route[N*i] = getGCD(route[N*i-N],A[N*i]);
+		}
+		int temp;
+		for (int i = 2; i<N-1; i++){
+			for(int j = 2; j<N-1; j++){
+				temp = A[N*i + j];
+				route[N*i+j] = Math.max(getGCD(route[N*i + j-N],temp),getGCD(route[N*i + j-1],temp));
+			}
+		}
+		return route[N*N-1];
 
 	}
+
+	public static int getArrGCD(int[] arr){
+		int length = arr.length;
+		int tempGCD = arr[0];
+		for (int i = 1; i < length; i++){
+			tempGCD = getGCD(tempGCD, arr[i]);
+		}
+		return tempGCD;
+	}
+
 	public static int getGCD(int a, int b) {
 		if (b==0) return a;
    		return getGCD(b,a%b);
-}
+	}
+
+	public static void main(String[] args) {
+		int n1 = 3;
+		int[] arr1 = { 4, 9, 2, 3, 5, 7, 8, 1, 6 };
+
+		int n2 = 5;
+		int[] arr2  = { 100,  80,  64,  48,  36, 75,  10,  10,  10,  48, 50,  10,  10,  10,  64, 25,  10,  10,  10,  80, 5,  25,  50,  75, 100 };
+
+		System.out.println(largestGCD(n1,arr1));
+		System.out.println(largestGCD(n2,arr2));
+
+	}
 }
